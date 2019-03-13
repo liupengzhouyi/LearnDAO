005de95d00e3339b92a92bade284834781a63eab
liupeng <LIUPENG.0@outlook.com>
2019-03-13 19:44:38 +0800

    注册用户.保存注册用户 V1.0.8 注册用户.测试保存类 V1.0.9

--- src/ExpressionLanguage/Registered/SaveUserData.java
+++ src/ExpressionLanguage/Registered/SaveUserData.java
@@ -2,6 +2,41 @@ package ExpressionLanguage.Registered;
 
 public class SaveUserData {
 
+    private User user = null;
 
+    private String sql = null;
+
+    public SaveUserData(User user) {
+        this.init();
+        //注册用户
+        this.setUser(user);
+        //转化SQL语句
+        this.setSql();
+    }
+
+    /**
+     * 初始化
+     */
+    public void init() {
+        this.user = new User();
+        this.sql = new String();
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getSql() {
+        return sql;
+    }
+
+    public void setSql() {
+        User user = this.getUser();
+        this.sql = "insert into el_user_table(user_name, user_password) value (\'" + user.getUser_name() + "\', \'" + user.getUser_password() + "\');";
+    }
 
 }
---
+++ test/ExpressionLanguage/Registered/SaveUserDataTest.java
@@ -0,0 +1,33 @@
+package ExpressionLanguage.Registered;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SaveUserDataTest {
+
+    private SaveUserData saveUserData = null;
+
+    @Before
+    public void setUp() throws Exception {
+        User user = new User();
+        user.setUser_id("123");
+        user.setUser_name("liupeng");
+        user.setUser_password("12345678");
+        System.out.println(user.getUser_name());
+        System.out.println(user.getUser_password());
+        System.out.println(user.getUser_id());
+        saveUserData = new SaveUserData(user);
+    }
+
+    @Test
+    public void say() {
+        System.out.println(saveUserData.getSql());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
