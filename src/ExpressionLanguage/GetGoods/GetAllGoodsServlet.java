e11d38e6b186a90539956bb9decce7f91f28dc31
liupeng <LIUPENG.0@outlook.com>
2019-03-14 16:24:47 +0800

    获取货物数据.单元测试-获取数据库数据,到Session V 1.0.2

---
+++ src/ExpressionLanguage/GetGoods/GetAllGoodsServlet.java
@@ -0,0 +1,44 @@
+package ExpressionLanguage.GetGoods;
+
+import ExpressionLanguage.AddGoods.Good;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+@WebServlet(name = "GetAllGoodsServlet", urlPatterns = "/ExpressionLanguage/GetGoods/GetAllGoodsServlet")
+public class GetAllGoodsServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            List<Good> list = new GetAllGoods().getListOfGoods();
+
+            if (list.size() == 0) {
+                // 没有取到数据
+                // 走错误页面
+                response.sendRedirect("/ExpressionLanguage/ErrorPages/GetGoodsErrorPage.jsp");
+            } else {
+                // 保存Session
+                HttpSession httpSession = request.getSession();
+                httpSession.setAttribute("goodList", list);
+                //跳转详情查看页面
+                System.out.println("11111");
+                response.sendRedirect("/ExpressionLanguage/ErrorPages/GetGoodsErrorPage.jsp");
+            }
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+}
---
+++ web/ExpressionLanguage/ErrorPages/GetGoodsErrorPage.jsp
@@ -0,0 +1,22 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: liupeng
+  Date: 2019-03-14
+  Time: 16:19
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>错误页面</title>
+</head>
+<body>
+    <h1>
+        亲爱的用户，您好！我是您爸爸！
+    </h1>
+    <hr>
+    <h2>
+        很抱歉，我们不可以为您服务了，因为您爸爸的创造者写下来Bug！目前我也不知道知道是什么原因，找不到您要的数据！
+    </h2>
+</body>
+</html>
