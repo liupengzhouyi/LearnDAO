cd07b9f795bf99e90376bcfb82e02b621bfef8a4
liupeng <LIUPENG.0@outlook.com>
2019-03-15 20:30:10 +0800

    提交订单，验证登陆 V 1.0.3

--- src/ExpressionLanguage/ShowGoods/GoodInformationServlet.java
+++ src/ExpressionLanguage/ShowGoods/GoodInformationServlet.java
@@ -2,6 +2,7 @@ package ExpressionLanguage.ShowGoods;
 
 import ExpressionLanguage.AddGoods.Good;
 import ExpressionLanguage.GetGoods.JudgeGoodId;
+import ExpressionLanguage.Registered.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,45 +16,57 @@ import java.sql.SQLException;
 @WebServlet(name = "GoodInformationServlet", urlPatterns = "/ExpressionLanguage/ShowGoods/GoodInformationServlet")
 public class GoodInformationServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("商品详情页");
+        //获取Session对象
+        HttpSession httpSession = request.getSession();
+        /*System.out.println("商品详情页");*/
+        boolean key1 = false;
+        //验证用户的有效性
+        //获取Session中的用户数据
+        String user_id = (String)httpSession.getAttribute("user_id");
+        //System.out.println("user_id : " + user_id);
+        if (user_id == null) {
+            //该用户未登陆
+            //转登陆页面
+            System.out.println("选择商品的用户没有登陆！");
+            //页面转发
+            response.sendRedirect("/ExpressionLanguage/ErrorPages/NotLanding.jsp");
+        } else {
+            //获取用户传入的商品ID号
+            String good_id = request.getParameter("good_id");
+
+            //验证改商品ID的有效性
+            try {
+                JudgeGoodId judgeGoodId = new JudgeGoodId(good_id);
+
+                boolean key = judgeGoodId.isKey();
+
+                //如果有改商品ID，显示改商品的详细信息
+                if (key) {
 
 
-        //获取用户传入的商品ID号
-        String good_id = request.getParameter("good_id");
+                    //清除原来在Session中的商品信息
+                    httpSession.removeAttribute("good_information");
 
-        //验证改商品ID的有效性
-        try {
-            JudgeGoodId judgeGoodId = new JudgeGoodId(good_id);
+                    Good good = judgeGoodId.getGood();
+                    //保存新的商品信息到Session中
 
-            boolean key = judgeGoodId.isKey();
+                    httpSession.setAttribute("good_information", good);
 
-            //如果有改商品ID，显示改商品的详细信息
-            if (key) {
+                    //页面转发
+                    response.sendRedirect("/ExpressionLanguage/ShowOrder/HTMLFlie/index.jsp");
 
-                HttpSession httpSession = request.getSession();
-                //清除原来在Session中的商品信息
-                httpSession.removeAttribute("good_information");
+                } else {
+                    //如果没有该商品的信息，显示错误提示
 
-                Good good = judgeGoodId.getGood();
-                //保存新的商品信息到Session中
+                    response.sendRedirect("/ExpressionLanguage/ErrorPages/ErrorOfGoodPages.jsp");
+                }
 
-                httpSession.setAttribute("good_information", good);
-
-                //页面转发
-                response.sendRedirect("/ExpressionLanguage/ShowOrder/HTMLFlie/index.jsp");
-
-            } else {
-                //如果没有该商品的信息，显示错误提示
-
-                response.sendRedirect("/ExpressionLanguage/ErrorPages/ErrorOfGoodPages.jsp");
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
-
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
-            e.printStackTrace();
         }
-
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
---
+++ web/ExpressionLanguage/ErrorPages/NotLanding.jsp
@@ -0,0 +1,36 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: liupeng
+  Date: 2019-03-15
+  Time: 20:16
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>用户未登陆</title>
+</head>
+<body>
+    <h1>
+        亲爱的用户，您好！您没有登陆，我们无法为您提供服务！
+    </h1>
+
+    <h2>
+        请您登陆或者注册
+    </h2>
+
+    <hr>
+
+    <a href="/ExpressionLanguage/Landing/HTMLFlies/index.jsp">
+        <p>
+            登陆
+        </p>
+    </a>
+
+    <a href="/ExpressionLanguage/Registered/HTMLFile/index.jsp">
+        <p>
+            注册
+        </p>
+    </a>
+</body>
+</html>
